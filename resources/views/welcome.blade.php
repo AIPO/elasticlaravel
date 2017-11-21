71a8a6c7f5c5e6910ff36891a54e171e8f90efdf
povilas <p.vaitonis@gmail.com>
2017-11-21 22:08:10 +0200

    Cleared code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,23 +13,6 @@
         <!-- Styles -->
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
                 <form id="elasticScout" action="/SearchQuery" method="get">
                     <div class="mysearchbar">
                         <input name="search" placeholder="Search...">
